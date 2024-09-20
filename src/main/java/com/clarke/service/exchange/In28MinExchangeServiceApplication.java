933cbd6fe7c09aa6ed2944c5d85f1971dcbf91bd
clarkesw <clarke.job@gmail.com>
2024-09-20 18:50:30 -0400

    First Time

---
+++ src/main/java/com/clarke/service/exchange/In28MinExchangeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.clarke.service.exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class In28MinExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(In28MinExchangeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clarke/service/exchange/In28MinExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clarke.service.exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class In28MinExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
