5aa7e9bbfc181667fe5e346412003e7fc0d7c02a
Clarke <clarke@HP-Laptop>
2024-09-23 14:12:52 -0400

    Added Eureka

--- src/main/java/com/clarke/service/exchange/ExchangeApp.java
+++ src/main/java/com/clarke/service/exchange/ExchangeApp.java
@@ -3,11 +3,10 @@ package com.clarke.service.exchange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class In28MinExchangeServiceApplication {
-
+public class ExchangeApp {
     public static void main(String[] args) {
-        SpringApplication.run(In28MinExchangeServiceApplication.class, args);
+        SpringApplication.run(ExchangeApp.class, args);
     }
-
 }
