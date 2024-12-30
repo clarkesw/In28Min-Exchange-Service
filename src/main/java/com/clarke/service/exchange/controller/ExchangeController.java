67eac05c3c23bcae81162fcf61cf41767ac793e3
clarkesw <clarke.job@gmail.com>
2024-12-30 10:03:22 -0500

    H2 DB working

--- src/main/java/com/clarke/service/exchange/controller/ExchangeController.java
+++ src/main/java/com/clarke/service/exchange/controller/ExchangeController.java
@@ -2,7 +2,8 @@ package com.clarke.service.exchange.controller;
 
 import com.clarke.service.exchange.beans.Exchange;
 import com.clarke.service.exchange.repository.CurrencyExchangeRepository;
-import java.math.BigDecimal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,10 +19,14 @@ public class ExchangeController {
     @Autowired
     private CurrencyExchangeRepository repo;
 
+    private final Logger log = LoggerFactory.getLogger(ExchangeController.class);
+                
     @GetMapping("/currency-exchange/from/{startCurrency}/to/{endCurrency}")
     public Exchange getConversionInfo(
-                @PathVariable String startCurrency, 
-                @PathVariable String endCurrency){
+                        @PathVariable String startCurrency, 
+                        @PathVariable String endCurrency){
+        
+        log.info("GET /currency-exchange from {} to {}", startCurrency, endCurrency);
         Exchange currencyConversion = 
                 repo.findByFromCurrencyAndToCurrency(startCurrency, endCurrency);  
         
